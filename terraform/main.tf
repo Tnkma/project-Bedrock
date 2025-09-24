9b74b4572825348f330cfa582f36f750f0519a02
Godswill <onwusilikegodswill@gmail.com>
2025-09-24 04:58:45 +0100

    moved tfstate to s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,19 @@
+# Terraform backend configuration
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "6.14.0"
-        }
+  backend "s3" {
+    bucket         = "bedrock-terraform-tfstate"
+    key            = "project-bedrock/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.14.0"
     }
+  }
 }
 
 provider "aws" {
